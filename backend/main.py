39d3cda18c9da59033cd0b596f57d9458797a39a
Grant <gsteere4717@xmail.dixietech.edu>
2024-04-04 10:38:09 -0600

    Starting api

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
+import uuid
+
 from fastapi import FastAPI
 
+from models.player import Player
+from models.course import Course
 
 app = FastAPI()
 
+players: dict[uuid.UUID, Player] = {}
+courses: dict[uuid.UUID, Course] = {}
 
 
+@app.get("/players")
+#@app.post("/players")
+#@app.put("/players/{player_id}")
+#@app.delete("/players/{player_id}")
+
+#@app.get("/courses")
+#@app.post("/courses")
+#@app.put("/courses/{course_id}")
+#@app.delete("/courses/{course_id}")
